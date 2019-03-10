1e352592fa34effd0ed4dabc2776ba2520d0144d
jgtorrejon <joel.torrejon.mendez@gmail.com>
2019-03-10 10:20:17 -0400

    Added template to cumpa project

--- cumpa/views.py
+++ cumpa/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is a cumpa test')
+    return render(request, 'cumpa/index.html')
