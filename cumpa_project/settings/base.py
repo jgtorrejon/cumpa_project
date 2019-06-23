c54d885ba349783a8db1ffb32a6cc02f984cdd33
jgtorrejon <joel.torrejon.mendez@gmail.com>
2019-06-23 12:59:58 -0400

    add deploy setup

--- cumpa_project/settings/base.py
+++ cumpa_project/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cumpa'
+    'cumpa',
+    'djangoup'
 ]
 
 MIDDLEWARE = [
