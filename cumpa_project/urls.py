ecc2ef85c884ee2fc209eba331bce156477467fc
Mauricio Ballesteros <mballes95@gmail.com>
2019-08-08 20:30:36 -0400

    Add URL for assets develop

--- cumpa_project/urls.py
+++ cumpa_project/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cumpa.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='session/login.html'))
 ]
+
+if settings.DEBUG:
+	from django.conf.urls.static import static
+
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
